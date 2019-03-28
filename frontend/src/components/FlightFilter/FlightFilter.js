2b677c9f3fe81f35cd0c8a90cd089397b7c51f84
BrijeshBumrela <brijeshbumrela@gmail.com>
2019-03-28 09:18:48 +0530

    Filter column complete

--- frontend/src/components/FlightFilter/FlightFilter.js
+++ frontend/src/components/FlightFilter/FlightFilter.js
@@ -1,25 +1,60 @@
 import React from 'react';
 
-import { Radio, Checkbox } from 'antd';
+import { Radio, Checkbox, Divider } from 'antd';
 
 import classes from './FlightFilter.module.css';
 
 const RadioGroup = Radio.Group;
 
-
 const flightFilter = (props) => {
     return (  
         <div className={classes.FilterColumn}>
             <div className={classes.FlightType}>
-                <h3>Flight Type</h3>
-                <RadioGroup>
-                    <Radio value={1}>Non-Stop</Radio>
+                <h2 className={classes.TypeHeader}>Flight Type</h2>
+                <RadioGroup className={classes.FlightOptions}>
+                    <Radio value={1}>Non-Stop</Radio><br />
                     <Radio value={2}>Stop</Radio>
-                </RadioGroup>
+                </RadioGroup>            
             </div>
 
-            <div>
-                <h3>Flight Timing</h3>
+            <Divider />
+
+            <div className={classes.FlightType}>
+                <h2 className={classes.TypeHeader}>Price Range</h2>
+                <p>
+                    <Checkbox 
+                        checked
+                        disabled={false}
+                    />
+                    <span>&lt; 3000</span>
+                </p>
+                <p>
+                    <Checkbox 
+                        checked
+                        disabled={false}
+                    />
+                    <span>&lt; 5000</span>
+                </p>
+                <p>
+                    <Checkbox 
+                        checked
+                        disabled={false}
+                    />
+                    <span>&lt; 10000</span>
+                </p>
+                <p>
+                    <Checkbox 
+                        checked
+                        disabled={false}
+                    />
+                    <span>&gt; 10000</span>
+                </p>
+            </div>
+
+            <Divider />
+
+            <div className={classes.FlightType}>
+                <h2 className={classes.TypeHeader}>Flight Time</h2>
                 <p>
                     <Checkbox 
                         checked
