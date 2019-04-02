37897a66f44fa5a227ea60949d60f3bd8defef7c
BrijeshBumrela <brijeshbumrela@gmail.com>
2019-04-03 00:42:45 +0530

    Added folder for containers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,29 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 import FlightSearch from './containers/FlightSearch/FlightSearch';
 
 import 'antd/dist/antd.css';
 
+import reducer from './store/reducer';
+
+const store = createStore(reducer);
+
 class App extends Component {
     render() {
         return (
-            <BrowserRouter>
-                <Switch>
-                    <Route path="/" exact render={() => <h1>Home</h1>} />
-                    <Route path="/flights" component={FlightSearch} />
-                    <Route path="/book-flight" render={() => <h1>Book Flight Page</h1>}/>
-                </Switch>
-            </BrowserRouter>
+            <Provider store={store}>
+                <BrowserRouter>
+                    <Switch>
+                        <Route path="/" exact render={() => <h1>Home</h1>} />
+                        <Route path="/flights" component={FlightSearch} />
+                        <Route path="/book-flight" render={() => <h1>Book Flight Page</h1>}/>
+                    </Switch>
+                </BrowserRouter>
+            </Provider>
         );
     }
 }
