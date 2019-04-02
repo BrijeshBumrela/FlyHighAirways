4eafc7d20de5e1c02073f7a76f9f57bb5d5b984f
adwait-thattey <coderdude1999@gmail.com>
2019-04-02 23:38:45 +0530

    add error handling utility functions

--- backend/utils/index.js
+++
@@ -1,4 +0,0 @@
-exports.database = require('./database');
-exports.syncdb = require('./syncdb');
-
-
--- backend/utils/syncdb.js
+++ backend/utils/syncdb.js
@@ -1,11 +1,13 @@
 const models = require('../models');
 const sequelize = require('./database');
 
-sequelize.sync({ "force": true }).then(result => {
-    console.log(result);
-    console.log("Models synced!", result.models);
-    console.log("db synced!");
-}).catch(err => {
-    console.log(err);
-    console.log("Sync Failed!");
-});
+// sequelize.sync({ "force": true }).then(result => {
+//     console.log(result);
+//     console.log("Models synced!", result.models);
+//     console.log("db synced!");
+// }).catch(err => {
+//     console.log(err);
+//     console.log("Sync Failed!");
+// });
+
+models.auth.User.createAll({"force":true});
