a767f28d1559aaf556710cfc21fdd95f2bdcf36d
BrijeshBumrela <brijeshbumrela@gmail.com>
2019-04-03 23:28:44 +0530

    API key secured

--- frontend/src/containers/Auth/Auth.js
+++ frontend/src/containers/Auth/Auth.js
@@ -35,14 +35,22 @@ class AuthenticateForm extends Component {
                 },
                 valid: false
             },
-        }
+        },
+        isSignUp: true
     }
 
     authHandler = (event) => {
         event.preventDefault();
-        console.log('submitted eh?');
-        this.props.onAuth(this.state.authForm.email.value, this.state.authForm.password.value);
+        this.props.onAuth(this.state.authForm.email.value, this.state.authForm.password.value, this.state.isSignUp);
     };
+
+    switchAuthModeHandler = () => {
+        this.setState(prevState => { 
+            return {
+                isSignUp: !prevState.isSignUp
+            };    
+        });
+    }
 
     checkValidation = (value, rules) => {
         let isValid = false;
@@ -74,27 +82,34 @@ class AuthenticateForm extends Component {
         const { authForm } = this.state;
 
         return (
-            <form className={classes.container} onSubmit={(e) => this.authHandler(e)}>
-                <Input 
-                    className={classes.InputElement} 
-                    prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} 
-                    type={authForm.email.elementConfig.type} 
-                    placeholder={authForm.email.elementConfig.placeholder} 
-                    value={authForm.email.value}
-                    onChange={(e) => this.inputChangedHandler(e, authForm.email.elementConfig.type)}
-                />
-                
-                <Input
-                    className={classes.InputElement} 
-                    prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} 
-                    type="password" 
-                    placeholder="Password" 
-                    value={authForm.password.value}
-                    onChange={(e) => this.inputChangedHandler(e, authForm.password.elementConfig.type)}
-                />
+            <div className={classes.container}>
+                <form onSubmit={(e) => this.authHandler(e)}>
+                    <Input 
+                        className={classes.InputElement} 
+                        prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} 
+                        type={authForm.email.elementConfig.type} 
+                        placeholder={authForm.email.elementConfig.placeholder} 
+                        value={authForm.email.value}
+                        onChange={(e) => this.inputChangedHandler(e, authForm.email.elementConfig.type)}
+                    />
+                    
+                    <Input
+                        className={classes.InputElement} 
+                        prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} 
+                        type="password" 
+                        placeholder="Password" 
+                        value={authForm.password.value}
+                        onChange={(e) => this.inputChangedHandler(e, authForm.password.elementConfig.type)}
+                    />
 
-                <Button htmlType="submit">Log In</Button>
-            </form>
+                    <Button htmlType="submit">
+                        { this.state.isSignUp ? 'Sign Up' : 'Log In' }
+                    </Button>
+                </form>
+                <Button type="ghost" onClick={this.switchAuthModeHandler}>
+                    { this.state.isSignUp ? 'Click Here To SignUp' : 'Click Here To LogIn' }
+                </Button>
+            </div>
         )
     }
 
@@ -102,7 +117,7 @@ class AuthenticateForm extends Component {
 
 const mapDispatchToProps = dispatch => {
     return {
-        onAuth: (email, password) => dispatch(actions.auth(email, password))
+        onAuth: (email, password, method) => dispatch(actions.auth(email, password, method))
     }
 }
 
--- frontend/src/store/actions/auth.js
+++ frontend/src/store/actions/auth.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { APIKEY } from '../../Keys/GoogleApiKey';
 
 import * as actionTypes from './actionTypes';
 
@@ -23,15 +24,22 @@ export const authFailure = (error) => {
     }
 };
 
-export const auth = (email, password) => {
+export const auth = (email, password, isSignUp) => {
     return dispatch => {
         dispatch(authStart());
         const authData = {
             email,
             password,
             returnSecureToken: true
+        };
+
+        let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=';
+        
+        if (isSignUp) {
+            url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=';
         }
-        axios.post('https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyC3K-JODbOgwSceKEZGoHb4mvP019-M4h0', authData)
+
+        axios.post(`${url}${APIKEY}`, authData)
             .then(response => {
                 console.log(response);
                 dispatch(authSuccess(response.data));
