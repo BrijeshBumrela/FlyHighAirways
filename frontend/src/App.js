2cb9cdf364ccd0fe46d8fa9391a71d8884a7670b
Aaquib Niaz <aaquibniaz3600@gmail.com>
2019-04-03 22:12:45 +0530

    added dynamic input fields

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,39 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 
-import FlightSearch from './containers/FlightSearch/FlightSearch';
-import HomePage from './containers/Homepage/Hompage';
-import Auth from './containers/Auth/Auth';
+import FlightSearch from "./containers/FlightSearch/FlightSearch";
+import HomePage from "./containers/Homepage/Hompage";
+import Auth from "./containers/Auth/Auth";
 
-import 'antd/dist/antd.css';
+import "antd/dist/antd.css";
 
-import reducer from './store/reducers/reducer';
+import reducer from "./store/reducers/reducer";
+import FlightBook from "./containers/FlightBook/FlightBook";
 
-const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 class App extends Component {
-    render() {
-        return (
-            <Provider store={store}>
-                <BrowserRouter>
-                    <Switch>
-                        <Route path="/" exact component={HomePage} />
-                        <Route path="/flights" component={FlightSearch} />
-                        {/* <Route path="/book-flight" /> */}
-                        <Route path="/authenticate" component={Auth} />
-                    </Switch>
-                </BrowserRouter>
-            </Provider>
-        );
-    }
+  render() {
+    return (
+      <Provider store={store}>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" exact component={HomePage} />
+            <Route path="/flights" component={FlightSearch} />
+            <Route path="/book-flight" component={FlightBook} />
+            {/* <Route path="/book-flight" /> */}
+            <Route path="/authenticate" component={Auth} />
+          </Switch>
+        </BrowserRouter>
+      </Provider>
+    );
+  }
 }
 
 export default App;
