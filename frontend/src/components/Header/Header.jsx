26322ee98d6fefedfa5af9fe63a59def248e2325
Aaquib Niaz <aaquibniaz3600@gmail.com>
2019-04-07 23:55:47 +0530

    working on drawer

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,35 +1,54 @@
 import React, { Component } from "react";
-import { Layout, Menu, Icon } from "antd";
+import { Layout, Menu, Icon, Button, Drawer } from "antd";
+import classes from "./Header.module.css";
 
 const { Header, Content, Footer, Sider } = Layout;
 const SubMenu = Menu.SubMenu;
 const MenuItemGroup = Menu.ItemGroup;
+
 class Navbar extends Component {
+  state = {
+    current: "mail",
+    visible: false
+  };
+  showDrawer = () => {
+    this.setState({
+      visible: true
+    });
+  };
+
+  onClose = () => {
+    this.setState({
+      visible: false
+    });
+  };
+
   render() {
     return (
       <nav
-        style={{
-          padding: "0 20px",
-          borderBottom: " solid 1px #e8e8e8",
-          overflow: "auto",
-          boxShadow: " 0 0 30px #f3f1f1"
-        }}
+        // style={{
+        //   padding: "0 20px",
+        //   borderBottom: " solid 1px #e8e8e8",
+        //   overflow: "auto",
+        //   boxShadow: " 0 0 30px #f3f1f1"
+        // }}
+        className={classes.menuBar}
       >
         <Header style={{ background: "#fff" }}>
           <div
-            className="logo"
-            style={{
-              display: "inline-block",
-              fontSize: "20px",
-              float: "left"
-            }}
+            className={classes.logo}
+            // style={{
+            //   display: "inline-block",
+            //   fontSize: "20px",
+            //   float: "left"
+            // }}
           >
             <a href="">
               <img
                 alt="logo"
                 src="https://t.alipayobjects.com/images/rmsweb/T1B9hfXcdvXXXXXXXX.svg"
                 height="64px"
-                style={{ display: "inline" }}
+                // style={{ display: "inline" }}
               />
             </a>
           </div>
@@ -99,6 +118,92 @@ class Navbar extends Component {
                 </MenuItemGroup>
               </SubMenu>
             </Menu>
+
+            <Button
+              className={classes.barsMenu}
+              type="primary"
+              onClick={this.showDrawer}
+              // style={{
+              //   float: "right",
+              //   display: "none",
+              //   background: "none"
+              // }}
+            >
+              <span className="barsBtn" />
+            </Button>
+            <Drawer
+              title="Basic Drawer"
+              placement="right"
+              closable={false}
+              onClose={this.onClose}
+              visible={this.state.visible}
+            >
+              <Menu
+              // theme="light"
+              // mode="horizontal"
+              // // defaultSelectedKeys={["2"]}
+              // overflowedIndicator={<Icon type="bars" />}
+              // style={{ lineHeight: "64px" }}
+              >
+                {/* <Menu.Item key="0" disbled>
+              <img
+                alt="logo"
+                src="https://t.alipayobjects.com/images/rmsweb/T1B9hfXcdvXXXXXXXX.svg"
+                height="64px"
+                style={{ display: "inline" }}
+              />
+            </Menu.Item> */}
+
+                <Menu.Item key="1">nav 1</Menu.Item>
+                <Menu.Item key="2">nav 2</Menu.Item>
+                <Menu.Item key="3">nav 3</Menu.Item>
+
+                <Menu.Item key="mail">
+                  <a href="">Home</a>
+                </Menu.Item>
+                <SubMenu title={<span>Blogs</span>}>
+                  <MenuItemGroup title="Item 1">
+                    <Menu.Item key="setting:1">Option 1</Menu.Item>
+                    <Menu.Item key="setting:2">Option 2</Menu.Item>
+                  </MenuItemGroup>
+                  <MenuItemGroup title="Item 2">
+                    <Menu.Item key="setting:3">Option 3</Menu.Item>
+                    <Menu.Item key="setting:4">Option 4</Menu.Item>
+                  </MenuItemGroup>
+                </SubMenu>
+                <Menu.Item key="alipay">
+                  <a href="#">Contact Us</a>
+                </Menu.Item>
+                {/* </Menu>
+          </div>
+          <div style={{ display: "inline-block", float: "right" }}>
+            <Menu
+              theme="light"
+              mode="horizontal"
+              // defaultSelectedKeys={["2"]}
+              overflowedIndicator={<Icon type="bars" />}
+              style={{ lineHeight: "64px" }}
+            > */}
+                <SubMenu
+                  style={{ float: "right" }}
+                  title={
+                    <span>
+                      <Icon type="setting" />
+                      Navigation Three - Submenu
+                    </span>
+                  }
+                >
+                  <MenuItemGroup title="Item 1">
+                    <Menu.Item key="setting:1">Option 1</Menu.Item>
+                    <Menu.Item key="setting:2">Option 2</Menu.Item>
+                  </MenuItemGroup>
+                  <MenuItemGroup title="Item 2">
+                    <Menu.Item key="setting:3">Option 3</Menu.Item>
+                    <Menu.Item key="setting:4">Option 4</Menu.Item>
+                  </MenuItemGroup>
+                </SubMenu>
+              </Menu>
+            </Drawer>
           </div>
           {/* </div> */}
         </Header>
