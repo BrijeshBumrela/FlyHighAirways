701b622070a54d8d85a01dabb7ca2b63616fd8be
Aaquib Niaz <aaquibniaz3600@gmail.com>
2019-04-08 00:13:57 +0530

    working on drawer

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -25,24 +25,9 @@ class Navbar extends Component {
 
   render() {
     return (
-      <nav
-        // style={{
-        //   padding: "0 20px",
-        //   borderBottom: " solid 1px #e8e8e8",
-        //   overflow: "auto",
-        //   boxShadow: " 0 0 30px #f3f1f1"
-        // }}
-        className={classes.menuBar}
-      >
+      <nav className={classes.menuBar}>
         <Header style={{ background: "#fff" }}>
-          <div
-            className={classes.logo}
-            // style={{
-            //   display: "inline-block",
-            //   fontSize: "20px",
-            //   float: "left"
-            // }}
-          >
+          <div className={classes.logo}>
             <a href="">
               <img
                 alt="logo"
@@ -52,15 +37,17 @@ class Navbar extends Component {
               />
             </a>
           </div>
-          <div style={{ display: "block" }}>
-            <Menu
-              theme="light"
-              mode="horizontal"
-              // defaultSelectedKeys={["2"]}
-              overflowedIndicator={<Icon type="bars" />}
-              style={{ lineHeight: "64px" }}
-            >
-              {/* <Menu.Item key="0" disbled>
+
+          <div className={classes.menuCon}>
+            <div className={classes.leftMenu}>
+              <Menu
+                theme="light"
+                mode="horizontal"
+                // defaultSelectedKeys={["2"]}
+                overflowedIndicator={<Icon type="bars" />}
+                style={{ lineHeight: "64px" }}
+              >
+                {/* <Menu.Item key="0" disbled>
               <img
                 alt="logo"
                 src="https://t.alipayobjects.com/images/rmsweb/T1B9hfXcdvXXXXXXXX.svg"
@@ -69,56 +56,59 @@ class Navbar extends Component {
               />
             </Menu.Item> */}
 
-              <Menu.Item key="1">nav 1</Menu.Item>
-              <Menu.Item key="2">nav 2</Menu.Item>
-              <Menu.Item key="3">nav 3</Menu.Item>
+                <Menu.Item key="1">nav 1</Menu.Item>
+                <Menu.Item key="2">nav 2</Menu.Item>
+                <Menu.Item key="3">nav 3</Menu.Item>
 
-              <Menu.Item key="mail">
-                <a href="">Home</a>
-              </Menu.Item>
-              <SubMenu title={<span>Blogs</span>}>
-                <MenuItemGroup title="Item 1">
-                  <Menu.Item key="setting:1">Option 1</Menu.Item>
-                  <Menu.Item key="setting:2">Option 2</Menu.Item>
-                </MenuItemGroup>
-                <MenuItemGroup title="Item 2">
-                  <Menu.Item key="setting:3">Option 3</Menu.Item>
-                  <Menu.Item key="setting:4">Option 4</Menu.Item>
-                </MenuItemGroup>
-              </SubMenu>
-              <Menu.Item key="alipay">
-                <a href="#">Contact Us</a>
-              </Menu.Item>
-              {/* </Menu>
-          </div>
-          <div style={{ display: "inline-block", float: "right" }}>
-            <Menu
-              theme="light"
-              mode="horizontal"
-              // defaultSelectedKeys={["2"]}
-              overflowedIndicator={<Icon type="bars" />}
-              style={{ lineHeight: "64px" }}
-            > */}
-              <SubMenu
-                style={{ float: "right" }}
-                title={
-                  <span>
-                    <Icon type="setting" />
-                    Navigation Three - Submenu
-                  </span>
-                }
+                <Menu.Item key="mail">
+                  <a href="">Home</a>
+                </Menu.Item>
+                <SubMenu title={<span>Blogs</span>}>
+                  <MenuItemGroup title="Item 1">
+                    <Menu.Item key="setting:1">Option 1</Menu.Item>
+                    <Menu.Item key="setting:2">Option 2</Menu.Item>
+                  </MenuItemGroup>
+                  <MenuItemGroup title="Item 2">
+                    <Menu.Item key="setting:3">Option 3</Menu.Item>
+                    <Menu.Item key="setting:4">Option 4</Menu.Item>
+                  </MenuItemGroup>
+                </SubMenu>
+                <Menu.Item key="alipay">
+                  <a href="#">Contact Us</a>
+                </Menu.Item>
+              </Menu>
+            </div>
+            <div
+              className={classes.rightMenu}
+              // style={{ display: "inline-block", float: "right" }}
+            >
+              <Menu
+                theme="light"
+                mode="horizontal"
+                // defaultSelectedKeys={["2"]}
+                overflowedIndicator={<Icon type="bars" />}
+                style={{ lineHeight: "64px" }}
               >
-                <MenuItemGroup title="Item 1">
-                  <Menu.Item key="setting:1">Option 1</Menu.Item>
-                  <Menu.Item key="setting:2">Option 2</Menu.Item>
-                </MenuItemGroup>
-                <MenuItemGroup title="Item 2">
-                  <Menu.Item key="setting:3">Option 3</Menu.Item>
-                  <Menu.Item key="setting:4">Option 4</Menu.Item>
-                </MenuItemGroup>
-              </SubMenu>
-            </Menu>
-
+                <SubMenu
+                  style={{ float: "right" }}
+                  title={
+                    <span>
+                      <Icon type="setting" />
+                      Navigation Three - Submenu
+                    </span>
+                  }
+                >
+                  <MenuItemGroup title="Item 1">
+                    <Menu.Item key="setting:1">Option 1</Menu.Item>
+                    <Menu.Item key="setting:2">Option 2</Menu.Item>
+                  </MenuItemGroup>
+                  <MenuItemGroup title="Item 2">
+                    <Menu.Item key="setting:3">Option 3</Menu.Item>
+                    <Menu.Item key="setting:4">Option 4</Menu.Item>
+                  </MenuItemGroup>
+                </SubMenu>
+              </Menu>
+            </div>
             <Button
               className={classes.barsMenu}
               type="primary"
@@ -129,7 +119,7 @@ class Navbar extends Component {
               //   background: "none"
               // }}
             >
-              <span className="barsBtn" />
+              <span className={classes.barsBtn} />
             </Button>
             <Drawer
               title="Basic Drawer"
