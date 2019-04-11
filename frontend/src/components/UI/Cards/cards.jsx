1bc39e1529167a238348c299a1f4f00a215673e6
Devil30 <ronytowilson3097@gmail.com>
2019-04-11 23:49:44 +0530

    added image overlay

--- frontend/src/components/UI/Cards/cards.jsx
+++ frontend/src/components/UI/Cards/cards.jsx
@@ -19,13 +19,17 @@ class Cards extends Component {
               bordered="true"
               style={{ with: 240 }}
               cover={
-                <img
-                  alt="example"
-                  src="https://scoutmytrip.com/blog/wp-content/uploads/2018/04/Places-to-visit-in-Darjeeling-Featured.jpg"
-                />
+                <div className={classes.container}>
+                  <img
+                    src="https://scoutmytrip.com/blog/wp-content/uploads/2018/04/Places-to-visit-in-Darjeeling-Featured.jpg"
+                    alt="Avatar"
+                    className={classes.image}
+                  />
+                  <div className={classes.overlay} />
+                </div>
               }
             >
-              <Meta title="Darjeeling" />
+              <Meta title="Darjeeling" style={{ fontFamily: "sans" }} />
             </Card>
           </Col>
           <Col span={6}>
@@ -34,13 +38,17 @@ class Cards extends Component {
               bordered="true"
               style={{ with: 240 }}
               cover={
-                <img
-                  alt="example"
-                  src="https://i.pinimg.com/originals/71/41/ff/7141fff9e2dfc82f159c9496dac3747b.jpg"
-                />
+                <div className={classes.container}>
+                  <img
+                    src="https://i.pinimg.com/originals/71/41/ff/7141fff9e2dfc82f159c9496dac3747b.jpg"
+                    alt="Avatar"
+                    className={classes.image}
+                  />
+                  <div className={classes.overlay} />
+                </div>
               }
             >
-              <Meta title="Jaipur" />
+              <Meta title="Jaipur" style={{ fontFamily: "sans" }} />
             </Card>
           </Col>
           <Col span={6}>
@@ -49,13 +57,17 @@ class Cards extends Component {
               bordered="true"
               style={{ with: 240 }}
               cover={
-                <img
-                  alt="example"
-                  src="https://scoutmytrip.com/blog/wp-content/uploads/2018/05/Hill-stations-in-India-Featured.jpg"
-                />
+                <div className={classes.container}>
+                  <img
+                    src="https://scoutmytrip.com/blog/wp-content/uploads/2018/05/Hill-stations-in-India-Featured.jpg"
+                    alt="Avatar"
+                    className={classes.image}
+                  />
+                  <div className={classes.overlay} />
+                </div>
               }
             >
-              <Meta title="Shillong" />
+              <Meta title="Shillong" style={{ fontFamily: "sans" }} />
             </Card>
           </Col>
           <Col span={6}>
@@ -64,13 +76,17 @@ class Cards extends Component {
               bordered="true"
               style={{ with: 240 }}
               cover={
-                <img
-                  alt="example"
-                  src="http://alldatmatterz.com/img/article/764/%E0%A4%95%E0%A5%81%E0%A4%B2%E0%A5%8D%E0%A4%B2%E0%A5%82%20%E0%A4%AE%E0%A4%A8%E0%A4%BE%E0%A4%B2%E0%A5%80.jpg"
-                />
+                <div className={classes.container}>
+                  <img
+                    src="http://alldatmatterz.com/img/article/764/%E0%A4%95%E0%A5%81%E0%A4%B2%E0%A5%8D%E0%A4%B2%E0%A5%82%20%E0%A4%AE%E0%A4%A8%E0%A4%BE%E0%A4%B2%E0%A5%80.jpg"
+                    alt="Avatar"
+                    className={classes.image}
+                  />
+                  <div className={classes.overlay} />
+                </div>
               }
             >
-              <Meta title="Manali" />
+              <Meta title="Manali" style={{ fontFamily: "sans" }} />
             </Card>
           </Col>
         </Row>
