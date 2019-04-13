1eaa7511e76fda21ac9c38d9253346cf715567d5
adwait-thattey <coderdude1999@gmail.com>
2019-04-13 22:30:55 +0530

    refactored database and tasks in another folder

--- backend/utils/syncdb.js
+++ backend/utils/syncdb.js
@@ -1,16 +1,25 @@
 const models = require('../models');
-const sequelize = require('./database');
+// const sequelize = require('./database/connect');
 
-// sequelize.sync({ "force": true }).then(result => {
-//     console.log(result);
-//     console.log("Models synced!", result.models);
-//     console.log("db synced!");
-// }).catch(err => {
-//     console.log(err);
-//     console.log("Sync Failed!");
-// });
-
-
-models.auth.User.createAll({"force":true}).then(result=>{
-    models.auth.OutstandingToken.createAll({"force":true});
+/*
+sequelize.sync({ "force": true }).then(result => {
+    console.log(result);
+    console.log("Models synced!", result.models);
+    console.log("db synced!");
+}).catch(err => {
+    console.log(err);
+    console.log("Sync Failed!");
 });
+*/
+
+
+models.auth.User.createAll({ "force": true }).then(result => {
+    return models.auth.OutstandingToken.createAll({ "force": true });
+}).then(result => {
+    return models.scheduledTask.ScheduledTask.createAll({ "force": true });
+}).then(result => {
+    return models.scheduledTask.CronLog.createAll({ "force": true });
+}).catch(err => {
+    console.log(err, "\nERRORED!")
+});
+
